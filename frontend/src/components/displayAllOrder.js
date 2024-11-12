4d76aae92bc27d3e52ee53dbab9c62bf9498b0f8
Nurzarina <nurzarina_os@yahoo.com>
2024-11-12 23:10:22 +0800

    Update UI

--- frontend/src/components/displayAllOrder.js
+++ frontend/src/components/displayAllOrder.js
@@ -1,33 +1,43 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function DisplayAllOrder() {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-      axios.get("http://localhost:5000/")
-        .then(res => setOrders(res.data))
-        .catch(err => console.log(err));
+    axios.get("http://localhost:5000/")
+      .then(res => setOrders(res.data))
+      .catch(err => console.log(err));
   }, []);
 
   const handleDelete = async (id) => {
     try {
-          await axios.delete('http://localhost:5000/order/' + id)
-          
-          //Re-fetch the updated orders without reloading the whole page.
-          const updatedOrders = await axios.get('http://localhost:5000/orders');
-          setOrders(updatedOrders.data); //Update state to trigger re-render
-        }
+      await axios.delete('http://localhost:5000/order/' + id)
+
+      //Re-fetch the updated orders without reloading the whole page.
+      const updatedOrders = await axios.get('http://localhost:5000/orders');
+      setOrders(updatedOrders.data); //Update state to trigger re-render
+    }
     catch (error) {
-          console.log('Error deleting order:', error)
+      console.log('Error deleting order:', error)
     }
   }
   return (
-    <div>
-      This is DisplayAllOrders
-      <ol>
-
-      </ol>
-    </div>
+    <Container>
+      <Card>
+        <Card.Header>
+          <h1>Orders</h1>
+        </Card.Header>
+        <Card.Body>
+          <Link to={"/addOrder"}>
+            <Button variant='info' className='mb-3'>
+              Add Order
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </Container>
   )
 }
 
